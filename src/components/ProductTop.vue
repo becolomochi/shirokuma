971df91ffec6d35c5857d6401166082abec30e6e
becolomochi <becolomochi@gmail.com>
2017-09-06 22:50:48 +0900

    作品ページ一覧作成

--- src/components/ProductTop.vue
+++ src/components/ProductTop.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
     <h1>Product</h1>
-    <product-list></product-list>
+    <product-list :data="myData"></product-list>
   </div>
 </template>
 
 <script>
   import Child from '@/components/parts/ProductList'
+  import myData from '../../static/data.json'
   export default {
     title: 'Product',
     components: {
       productList: Child
+    },
+    data () {
+      return {
+        myData: myData
+      }
     }
   }
 </script>
--- src/components/parts/ProductList.vue
+++ src/components/parts/ProductList.vue
@@ -1,13 +1,23 @@
 <template>
-  <article>
-    作品名
-    内容
-    制作年
-    タグ
-  </article>
+  <div>
+    <article class="product-box" v-for="d in data">
+      <div v-for="p in d.picture" v-if="p.id === '1'">
+        <img :src="p.path" width="" height="" alt="" class="product-image">
+      </div>
+      <h1 class="product-title"><router-link :to="'/product/' + d.slug">{{ d.title }}</router-link></h1>
+      <p class="product-description">{{ d.text }}</p>
+      <p class="product-year">制作年：{{ d.year }}</p>
+      <div class="product-tags">
+        <ul>
+          <li v-for="t in d.tags"><a href="#" class="tag">{{ t }}</a></li>
+        </ul>
+      </div>
+    </article>
+  </div>
 </template>
 
 <script>
   export default {
+    props: ['data']
   }
 </script>
