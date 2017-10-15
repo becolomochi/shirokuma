41fb36c0291e47db9141140ae26b4500607c1705
becolomochi <becolomochi@gmail.com>
2017-10-15 21:27:39 +0900

    refs #9 vue-analytics追加

--- src/main.js
+++ src/main.js
@@ -5,11 +5,16 @@ import App from './App'
 import titleMixin from './util/title'
 import descriptionMixin from './util/description'
 import router from './router'
+import VueAnalytics from 'vue-analytics'
 import './assets/style/style.scss'
 
 Vue.mixin(titleMixin)
 Vue.mixin(descriptionMixin)
 Vue.config.productionTip = false
+Vue.use(VueAnalytics, {
+  id: 'UA-22728284-8',
+  router
+})
 
 /* eslint-disable no-new */
 new Vue({
