b2fa7cb466dad95943855fbd57faf3be03b47e8a
becolomochi <becolomochi@gmail.com>
2019-09-08 06:00:33 +0900

    ナビゲーション修正

--- src/components/parts/NavHeader.vue
+++ src/components/parts/NavHeader.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
-  nav.header_nav
-    ul.header_nav-list
-      li.header_nav-item
-        router-link(to="/").header_nav-link Home
-      li.header_nav-item
-        router-link(to="/about").header_nav-link About
-      li.header_nav-item
-        router-link(to="/works").header_nav-link Works
-      li.header_nav-item
-        router-link(to="/contact").header_nav-link Contact
+  nav.nav
+    ul.nav_list
+      li.nav_item
+        router-link(to="/").nav_link Home
+      li.nav_item
+        router-link(to="/about").nav_link About
+      li.nav_item
+        router-link(to="/works").nav_link Works
+      li.nav_item
+        router-link(to="/contact").nav_link Contact
 </template>
